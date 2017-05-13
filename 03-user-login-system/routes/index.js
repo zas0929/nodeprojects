a4acabec106608884e0618420846965630958f52
zas0929 <rolisovskiy@mail.ru>
2017-05-13 22:16:37 +0300

    check

--- 03-user-login-system/routes/index.js
+++ 03-user-login-system/routes/index.js
@@ -6,11 +6,11 @@ router.get('/', ensureAuthenticated, function(req, res, next) {
   res.render('index', { title: 'Members' });
 });
 
-function ensureAuthenticated(req, res, next) {
-  if(req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect('/users/login');
+function ensureAuthenticated(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	}
+	res.redirect('/users/login');
 }
 
 module.exports = router;
